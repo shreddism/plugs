9dfcaa4e29757bb0e776b56f48cd70a272a4a018
shreddism <shreddism@protonmail.com>
2026-09-07 04:55:11 -0700

    noise again

--- dostuff/UHRRNC/UHRRNC.cs
+++ dostuff/UHRRNC/UHRRNC.cs
@@ -18,19 +18,40 @@ namespace UHRRNC
 
         private HPETDeltaStopwatch reportStopwatch = new HPETDeltaStopwatch();
 
-        [Property("Noise Amount"), DefaultPropertyValue(10f)]
+        [Property("Noise Amount"), DefaultPropertyValue(5f)]
         public float opt1 { 
             set => _opt1 = (value);
             get => _opt1;
         }
         public float _opt1;
 
-        [Property("Sensitivity"), DefaultPropertyValue(1f)]
+        [Property("Velocity Multiplier"), DefaultPropertyValue(0.99f)]
         public float opt2 { 
             set => _opt2 = (value);
             get => _opt2;
         }
         public float _opt2;
+
+        [Property("opt3"), DefaultPropertyValue(0.5f)]
+        public float opt3 { 
+            set => _opt3 = (value);
+            get => _opt3;
+        }
+        public float _opt3;
+
+        [Property("opt4"), DefaultPropertyValue(1f)]
+        public float opt4 { 
+            set => _opt4 = (value);
+            get => _opt4;
+        }
+        public float _opt4;
+
+        [Property("opt5"), DefaultPropertyValue(1f)]
+        public float opt5 { 
+            set => _opt5 = (value);
+            get => _opt5;
+        }
+        public float _opt5;
 
         public event Action<IDeviceReport> Emit;
 
@@ -48,6 +69,7 @@ namespace UHRRNC
                     init = true;
                     ldir = Vector2.Zero;
                     lpos = report.Position;
+                    paccel = Vector2.Zero;
                     return;
                 }
 
@@ -56,7 +78,7 @@ namespace UHRRNC
 
                     caccel = cdir - ldir;       
 
-                    float wale = 0.1f + 0.15f * Smoothstep(caccel.Length(), opt1 * 2f, opt1 * 4f) + 0.15f * Smoothstep(wtf.Length(), opt1 * 2f, opt1 * 4f);     
+                   /*float wale = 0.1f + 0.15f * Smoothstep(caccel.Length(), opt1 * 2f, opt1 * 4f) + 0.15f * Smoothstep(wtf.Length(), opt1 * 2f, opt1 * 4f);     
 
                     wale = inversion(wale, opt2);
 
@@ -78,12 +100,61 @@ namespace UHRRNC
 
                     opos = Vector2.Lerp(opos, report.Position, ale);
 
+                    Console.WriteLine(Vector2.Distance(opos, report.Position));*/
+
+                    wtf = Vector2.Lerp(wtf, caccel, inversion(0.1f, opt4));
+
+                    deepac = Vector2.Lerp(deepac, caccel, inversion(0.25f, opt4));
+
+                    paccel += wtf - caccel;
+
+                    paccel *= opt3;
+                    
+                    wtf += (Math.Max(Smoothstep(caccel.Length(), opt1 * 4f, opt1 * 6f), Smoothstep(paccel.Length(), opt1, opt1 * 3f))) * Math.Max(Vector2.Distance(wtf, caccel) - opt1 * 2f, 0f) * Normalize((0.67f * deepac + 0.33f * caccel) - wtf);
+
+                    adir += wtf * Smoothstep(wtf.Length(), opt1 * 0.25f, opt1);
+
+                    adir = Vector2.Lerp(adir, cdir, inversion(0.1f, opt4));
+
+                    dell = Vector2.Lerp(dell, cdir, inversion(0.25f, opt4));
+
+                    pdir += adir - cdir;
+
+                    pdir *= opt3;
+
+                    adir += Math.Max(Smoothstep(cdir.Length(), opt1 * 2f, opt1 * 3f), Smoothstep(pdir.Length(), opt1, opt1 * 1.5f)) * Math.Max(Vector2.Distance(adir, cdir) - opt1, 0f) * Normalize(cdir - adir);
+
+                    opos += opt2 * Smoothstep(adir.Length(), opt1 * 0.05f, opt1 * 0.33f) * adir;
+
+                    opos = Vector2.Lerp(opos, report.Position, inversion(0.1f * Smoothstep(adir.Length(), opt1 * 4f, 0f), opt4));
+
+                    ppos += opos - report.Position;
+                    
+                    ppos *= opt3;
+
+                    Vector2 finaldiff = 0.5f * Math.Max(Smoothstep(Vector2.Distance(opos, report.Position), opt1 * 1f, opt1 * 1.5f), Smoothstep(ppos.Length(), opt1 * 0.5f, opt1 * 2f)) * Math.Max(Vector2.Distance(opos, report.Position) - opt1 * 0.5f, 0f) * Normalize(report.Position - opos);
+
+                    opos += finaldiff;
+
+                    adir += 0.5f * finaldiff;
+
+                    wtf += 0.25f * finaldiff;
+
+                    if (opt5 < 1f) {
+                        smoo = Vector2.Lerp(smoo, opos, opt5);
+                    }
+                    else smoo = opos;
+
+                    //opos = Vector2.Lerp(opos, report.Position, (0.5f + 0.5f * Smoothstep(adir.Length(), 0f, opt1)) * Smoothstep(ppos.Length(), 0f, opt1 * (2f - Smoothstep(adir.Length() - dell.Length(), 0f, -25f))));
+                  Console.WriteLine(opos - report.Position);
+
                 }
                
                 lpos = report.Position;
                 ldir = cdir;
-                report.Position = opos;
-                lopos = opos;
+                report.Position = smoo;
+                odir = smoo - lopos;
+                lopos = smoo;
                 init = true;
             }
             Emit?.Invoke(value);
@@ -91,9 +162,12 @@ namespace UHRRNC
 
         Vector2 opos, odir, lpos, lopos, ldir, caccel, wtf;
         Vector2 cdir, adir = Vector2.Zero;
-        Vector2 cdisc, adisc;
+        Vector2 wisc;
+        Vector2 cdisc;
+        Vector2 ucel;
         Vector2 smoo;
-        float ppos, pdir, paccel;
+        Vector2 ppos, pdir, paccel;
+        Vector2 deepac, dell;
         bool init;
         bool bstate1, bstate2, bstate3;
 
@@ -123,6 +197,14 @@ namespace UHRRNC
         public static float inversion(float x, float pow) {
             return 1 - MathF.Pow(1 - x, pow);
         }
+
+        public static void InsertAtFirst<T>(T[] arr, T element)
+        {
+            for (int p = arr.Length - 1; p > 0; p--) arr[p] = arr[p - 1];
+            arr[0] = element;
+        }
+
+        public Vector2 Normalize(Vector2 a) => (a != Vector2.Zero) ? (a / a.Length()) : Vector2.Zero;
 
     }
 
